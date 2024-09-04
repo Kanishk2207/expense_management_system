f7f1e02725f9a8f46090a508a35a3f4170a0ce97
Kanishk <kanishk0422@gmail.com>
2024-09-04 17:21:15 +0530

    repo change

--- auth_service/internal/repository/user_repository.go
+++ auth_service/internal/repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"user-service/internal/models"
 )
 
 type UserRepositoy struct {
@@ -14,6 +15,24 @@ func NewUserRepository(db *sql.DB) *UserRepositoy {
 	return userRepoPtr
 }
 
-// func (r *UserRepositoy) CreateUser(user *models.User) error {
-// 	_, err := r.DB.Exec()
-// }
+func (r *UserRepositoy) CreateUser(user *models.User) error {
+	_, err := r.DB.Exec(
+		"INSERT INTO users (user_id, username, first_name, last_name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
+		user.UserID, user.Username, user.FirstName, user.LastName, user.Email, user.Password, user.CreatedAt, user.UpdatedAt,
+	)
+	return err
+}
+
+func (r *UserRepositoy) GetUserByEmail(email string) (*models.User, error) {
+	user := &models.User{}
+
+	err := r.DB.QueryRow(
+		"SELECT user_id, username, first_name, last_name, email, password FROM users WHERE email = ?", email,
+	).Scan(
+		&user.UserID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.Password,
+	)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	return user, err
+}
